a81964f3a2ece32cd9e851fade3b6e6318045288
Teemerte Teccola <teemerte.teccola@digital.trade.gov.uk>
2024-03-18 10:45:16 +0000

    update the app.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+// import './App.css';
 
 function App() {
+  const [count, setCount] =useState(0);
+  useEffect(()=> {
+    console.log("Counter has been initialised or updated.");
+  },[count])
+
+  function handleIncrement(){
+    setCount(prevState => prevState + 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Count: {count}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+  
     </div>
   );
 }
 
 export default App;
+
